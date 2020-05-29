57535a947c6c9c6ba307bb6d5e78398c96c091f1
Andres Rodriguez <andresrodriguez1026@hotmail.com>
2020-05-29 02:12:42 -0500

    Final styles added

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://A01193126:A01193126!@cluster0-uula5.mongodb.net/shoppingAppDB?retryWrites=true&w=majority",
-//exports.TOKEN = process.env.API_TOKEN || '123';
 exports.PORT = process.env.PORT || '8000';
 exports.SECRET_TOKEN = process.env.SECRET_TOKEN || 'WADfinalproject';
 exports.ADMIN_TOKEN = process.env.ADMIN_TOKEN || 'admin';
